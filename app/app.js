0915f9a61b701dc7c2466918ec7b62151b99655f
송짬민 <wogitopp@naver.com>
2021-08-16 09:15:39 +0000

    08161815 / API 작성

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@
 
 // < 모듈 >
 const express = require("express");  // express 모듈을 가져옴
+const bodyParser = require("body-parser");  // body-parser 모듈 가져옴
 const app = express();               // express를 실행시켜 app라는 변수에 넣어둠
                                      // -> app라는 이름으로 실제로 사용하게 됨
 
@@ -16,14 +17,16 @@ const PORT = 3000;                   // 포트 번호 상수로 선언
 const home = require("./src/routes/home");  // routing 분리해둔 js파일 읽어서 불러옴 (index.js)
 
 
-app.use("/", home);  // 미들웨어를 등록해주는 메서드.(?)
-                     // "/"로 접근하면 home(index.js)으로 넘어가서 해당 콜백함수를 동작시킴
-
-
-// < view 관리 >
+// 앱 세팅
 app.set("views", "./src/views");    // views를 세팅, view를 관리할 파일이 저장될 폴더의 경로를 지정
 app.set("view engine", "ejs");  // html 코드를 해석할 엔진을 정해줌(ejs : 가장 많이 쓰이는 view 엔진)
 app.use(express.static(`${__dirname}/src/public`));  // 현재 파일 경로 밑의 src/public 이라는 디렉터리를 정적 경로로 추가하는 코드 (미들웨어) ???? -> login.ejs에서 js폴더로 접근하게 되면 public 폴더로 가게 됨 (login.ejs에 login.js 연결하기 위한 코드)
+app.use(bodyParser.json());  // body-parser가 json 데이터를 파싱해올 수 있도록 명시 (미들웨어)
+app.use(bodyParser.urlencoded({ extended: true }))  // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제를 해결하기 위한 코드 (미들웨어)
+
+
+app.use("/", home);  // 미들웨어를 등록해주는 메서드.(?)
+                     // "/"로 접근하면 home(index.js)으로 넘어가서 해당 콜백함수를 동작시킴
 
 
 // app export
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,20 +2,31 @@
 
 "use strict";
 
+// 렌더링 관련 함수 객체
+const output = {
+    
+    // 루트로 접근할 시의 콜백함수 (단순 렌더링)
+    root_ctrl: (req, res) => {
+        res.render("home/index");      // views폴더의 home/index 파일을 화면에 띄움
+    },
 
-// 루트로 접근할 시의 콜백함수
-const root_ctrl = (req, res) => {
-    res.render("home/index");      // views폴더의 home/index 파일을 화면에 띄움
-};
+    // 로그인 화면으로 접근할 시의 콜백함수 (단순 렌더링)
+    login_ctrl: (req, res) => {
+        res.render("home/login");      // views폴더의 home/login 파일을 화면에 띄움
+    }
+}
 
-// 로그인 화면으로 접근할 시의 콜백함수
-const login_ctrl = (req, res) => {
-    res.render("home/login");      // views폴더의 home/login 파일을 화면에 띄움
-};
+// 로그인 처리 API 관련 함수 객체
+const process = {
+    login: (req, res) => {
+        console.log(req.body);
+    }
+}
+
 
 
 // Object로 export
 module.exports = {
-    root_ctrl,     // root에서의 컨트롤러를 사용하기 위해 export
-    login_ctrl,    // login 화면에서의 컨트롤러를 사용하기 위해 export
+    output,     // output 객체를 사용하기 위해 export
+    process     // process 객체를 사용하기 위해 export
 };
