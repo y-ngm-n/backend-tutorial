34376dbf7401b6f39ff789b6bb3a7ed46d7f7dc1
송짬민 <wogitopp@naver.com>
2021-09-07 11:00:25 +0000

    09072000 / create register page

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -17,6 +17,11 @@ const output = {
     // 로그인 화면으로 접근할 시의 콜백함수 (단순 렌더링)
     login_ctrl: (req, res) => {
         res.render("home/login");      // views폴더의 home/login 파일을 화면에 띄움
+    },
+    
+    // 회원가입 화면으로 접근할 시의 콜백함수 (단순 렌더링)
+    register_ctrl: (req, res) => {
+        res.render("home/register");
     }
 }
 
