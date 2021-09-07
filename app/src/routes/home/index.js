d576d27e9d76b8d02dca15dcab42f86e108980e9
송짬민 <wogitopp@naver.com>
2021-09-07 10:48:39 +0000

    09071948 / front design

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,9 +8,10 @@ const router = express.Router();      // routing을 따로 분리할 때 사용
 
 const ctrl = require("./home.ctrl");  // home.ctrl 모듈을 import
 
-// 
-router.get("/", ctrl.output.root_ctrl);        // root에 접근 시 응답할 동작 지정
-router.get("/login", ctrl.output.login_ctrl);  // login 화면에 접근 시 응답할 동작 지정
+// 라우팅
+router.get("/", ctrl.output.root_ctrl);             // root에 접근 시 응답할 동작 지정
+router.get("/login", ctrl.output.login_ctrl);       // login 화면에 접근 시 응답할 동작 지정
+router.get("/register", ctrl.output.register_ctrl); // register 화면에 접근 시 응답할 동작 지정
 
 router.post("/login", ctrl.process.login);  // login 기능을 처리하는 API
 
