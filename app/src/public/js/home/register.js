3ce7a0745308ec354073505597d2b91f876401ed
송짬민 <wogitopp@naver.com>
2021-09-07 13:53:08 +0000

    09072252 / register request

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,24 +1,28 @@
-// login 화면의 동작 담당하는 js (login.ejs와 연결)
+// register 화면의 동작 담당하는 js (register.ejs와 연결)
 
 "use strict";
 
-const id = document.querySelector("#id");           // id input 태그
-const pw = document.querySelector("#pw");           // pw input 태그
-const loginBtn = document.querySelector("#button");  // 버튼 태그
+const id = document.querySelector("#id");                  // id input 태그
+const name = document.querySelector("#name");              // name unput 태그
+const pw = document.querySelector("#pw");                  // pw input 태그
+const confirmPw = document.querySelector("#confirm-pw");  // confirm-pw input 태그
+const registerBtn = document.querySelector("#button");      // 버튼 태그
 
 
-loginBtn.addEventListener("click", lgn);  // 버튼 클릭할 시 lgn함수 실행
+registerBtn.addEventListener("click", rgst);  // 버튼 클릭할 시 lgn함수 실행
 
-function lgn() {
+function rgst() {
     const req = {
         id: id.value,
+        name: name.value,
         pw: pw.value,
+        confirmPw: confirmPw.value,
     };
     
     console.log(req, JSON.stringify(req));
     
     // fetch : 데이터를 보낼 경로와 보낼 데이터(객체)를 파라미터로 지정
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",  // http 메서드 중 POST 메서드로 데이터 전달해야함
         headers: {  // 보낼 데이터에 대한 설명으로 header를 작성
             "Content-Type": "application/json"  // json 데이터 전달 명시
@@ -28,12 +32,12 @@ function lgn() {
         .then((res) => res.json())  //then 메서드로 데이터를 받아옴
         .then((res) => {
             if (res.success) {
-                location.href = "/";
+                location.href = "/login";
             } else {
                 alert(res.msg);
             }
         })
         .catch((err) => {
-            console.error(new Error("로그인 중 에러 발생"));
+            console.error(new Error("회원가입 중 에러 발생"));
         });
 }
